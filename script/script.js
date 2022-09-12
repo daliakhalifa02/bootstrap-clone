645b91b7895dd21243d7697a990f167c7175a953
daliakhalifa02 <daliakhalifa02@gmail.com>
2022-09-13 02:06:40 +0300

    last script commit

--- script/script.js
+++ script/script.js
@@ -13,32 +13,6 @@ function validateMessage() {
      return false;
     }
  } 
- /*function myFunction() {
-    const inpObj = document.getElementById("message");
-    if (!inpObj.checkValidity()) {
-      document.getElementById("demo").innerHTML = inpObj.validationMessage;
-    } else {
-      document.getElementById("demo").innerHTML = "Input OK";
-    } 
-  } */
-
- /*function validateForm() {
-    let x=document.forms["myForm"]["name"].value;
-    let y=document.forms["myForm"]["message"].value;
-    if (x.length<5 || x==""){  
-        alert("Name must be filled out");  
-        return false;  
-    }
-    if (y.length<100){  
-        alert("Message must be at least 100 characters");  
-        return false;  
-    }  
-}*/
- //var btn = document.getElementById("send-button"); 
-     
-// Assigning event listeners to the button
-//btn.addEventListener("click", validateMessage); 
-//btn.addEventListener("click", validateName); 
 
 function validateEmail(){
     var allowed=/^\(?([a-zA-Z0-9._-]{3,6})\)?@[a-zA-Z0-9.-]{5}?.([a-zA-Z]{2,4})$/;
